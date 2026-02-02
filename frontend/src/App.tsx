3721c9dbc4a381fd757e620f4c67465ce0321bf5
Aman Kumar <aman.kumar.connect@gmail.com>
2026-02-02 10:16:57 +0530

    Auth UI

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,24 @@
+import { Routes, Route } from "react-router";
 import { Home } from "./pages/Home";
+import { Login } from "./pages/Login";
+import { StudentSignup } from "./pages/StudentSignup";
+import { FacultySignup } from "./pages/FacultySignup";
+import { StudentHome } from "./pages/StudentHome";
+import { FacultyHome } from "./pages/FacultyHome";
 
 export function App() {
-  return <Home />;
+  return (
+    <Routes>
+      <Route index element={<Home />} />
+      <Route path="login" element={<Login />} />
+      <Route path="signup">
+        <Route path="student" element={<StudentSignup />} />
+        <Route path="faculty" element={<FacultySignup />} />
+      </Route>
+      <Route path="student" element={<StudentHome />} />
+      <Route path="faculty" element={<FacultyHome />} />
+    </Routes>
+  );
 }
 
 export default App;
--- frontend/src/pages/StudentSignup.tsx
+++ frontend/src/pages/StudentSignup.tsx
@@ -1,3 +1,11 @@
+import { SignupForm } from "@/components/signup-form";
+
 export function StudentSignup() {
-  return <div>Student Signup Page</div>;
+  return (
+    <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
+      <div className="w-full max-w-sm">
+        <SignupForm role="Student" />
+      </div>
+    </div>
+  );
 }
